6f7569af6f8a4fc0d6f5717ce2717c7176a29703
Rafael Galvez <rfgalvez@msn.com>
2019-08-21 21:13:16 -0400

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,10 @@
 $(document).ready(function() { 
-// get input value on search bar
+// string of arrays 
+var friends=["Ross", "Joey", "Chandler", "Monica", "Phoebe", "Rachel"];
+var $imgBody=$('.img-body');
 var apiKey="Aq1A3tbXtbNHxiXCe1jpOz3aVv32tLq9";
 
+// click function on submit to retreive data from search bar
 $("#submit").click(function(event) {
     event.preventDefault();
   
@@ -13,8 +16,16 @@ function getGiphys(gifSearch) {
    
     $.get('http://api.giphy.com/v1/gifs/search?q=' + gifSearch + '&api_key='+ apiKey + '&limit=5')
         .done(function(data) {
-            console.log(data)
-        })
+            for (let i = 0; i < 5; i++) {
+                
+                console.log(data.data[i].images.downsized.url)
+                
+            }
+            })
     }
-
+function creatBox(){
+    var $img=$('img');
+    var $div=$('div');
+    $img.append($imgBody);
+}
 })
